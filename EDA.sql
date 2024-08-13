fd7d5eb71be5200d61e6850c16a36b7c612fce78
Rafiat Ajibode-Sosanya, MSc <167440482+Rapheehat@users.noreply.github.com>
2024-08-13 06:56:05 +0100

    Update EDA.sql

--- EDA.sql
+++ EDA.sql
@@ -10,7 +10,8 @@ ORDER BY incident_count DESC;
 SELECT route, COUNT(*) as incident_count
 FROM incidents
 GROUP BY route
-ORDER BY incident_count DESC;
+ORDER BY incident_count DESC
+LIMIT 10;
 
 -- Q3. How many incidents occurred each year?
 SELECT year, COUNT(*) as incident_count
@@ -40,20 +41,18 @@ GROUP BY v.victim_category
 ORDER BY victim_count DESC;
 
 -- Q7. What are the most common victim demographics (age and sex)?
-SELECT 
-    v.victims_sex, v.victims_age, COUNT(*) AS victim_count
-FROM
-    incidents i
-        JOIN
-    victims v ON i.victim_id = v.victim_id
+SELECT v.victims_sex, v.victims_age, COUNT(*) AS victim_count
+FROM incidents i
+JOIN victims v ON i.victim_id = v.victim_id
 GROUP BY v.victims_sex , v.victims_age
-ORDER BY victim_count DESC;
+ORDER BY v.victims_sex;
 
 -- Q8. Which boroughs have the highest number of incidents?
 SELECT borough, COUNT(*) as incident_count
 FROM incidents
 GROUP BY borough
-ORDER BY incident_count DESC;
+ORDER BY incident_count DESC
+LIMIT 20;
 
 -- Q9. Are there any bus garages that are associated with a higher number of incidents?
 SELECT g.bus_garage, COUNT(*) as incident_count
@@ -62,39 +61,20 @@ JOIN bus_garages g ON i.bus_garage_id = g.bus_garage_id
 GROUP BY g.bus_garage
 ORDER BY incident_count DESC;
 
--- Q10. On what dates did the most incidents occur?
-SELECT date_of_incident, COUNT(*) as incident_count
-FROM incidents
-GROUP BY date_of_incident
-ORDER BY incident_count DESC
-LIMIT 10;
-
--- Q11. Is there a particular month of the year when incidents are more common?
+-- Q10. Is there a particular month of the year when incidents are more common?
 SELECT MONTH(date_of_incident) as month, COUNT(*) as incident_count
 FROM incidents
 GROUP BY month
 ORDER BY incident_count DESC;
 
--- Q12. What is the distribution of incidents based on injury result description?
-SELECT injury_result_description, COUNT(*) as incident_count
+-- Q11. What is the distribution of incidents based on injury result description?
+SELECT injury_result_description, COUNT(*) as incident_count,
+  ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM incidents), 1) as percentage
 FROM incidents
 GROUP BY injury_result_description
 ORDER BY incident_count DESC;
 
--- Q13. Is there a correlation between the type of incident and the category of victim?
-SELECT i.incident_event_type, v.victim_category, COUNT(*) as incident_count
-FROM incidents i
-JOIN victims v ON i.victim_id = v.victim_id
-GROUP BY i.incident_event_type, v.victim_category
-ORDER BY incident_count DESC;
-
--- Q14. How has the number of incidents changed over the months in each year?
-SELECT year, MONTH(date_of_incident) as month, COUNT(*) as incident_count
-FROM incidents
-GROUP BY year, month
-ORDER BY year, month;
-
--- Q15. How do incident rates vary between different times of the year (seasons)?
+-- Q12. How do incident rates vary between different times of the year (seasons)?
 SELECT 
   CASE 
     WHEN MONTH(date_of_incident) IN (12, 1, 2) THEN 'Winter'
@@ -107,22 +87,14 @@ FROM incidents
 GROUP BY season
 ORDER BY incident_count DESC;
 
--- Q16. What proportion of incidents result in severe injuries?
-SELECT 
-  injury_result_description, 
-  COUNT(*) as incident_count,
-  ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM incidents), 1) as percentage
-FROM incidents
-GROUP BY injury_result_description;
-
--- Q17. What is the age distribution of victims involved in incidents?
+-- Q13. What is the age distribution of victims involved in incidents?
 SELECT victims_age, COUNT(*) as victim_count
 FROM victims v
 JOIN incidents i ON v.victim_id = i.victim_id
 GROUP BY victims_age
 ORDER BY victim_count DESC;
 
--- Q18. What is the average number of incidents per route?
+-- Q14. What is the average number of incidents per route?
 SELECT AVG(incident_count) as average_incidents_per_route
 FROM (
   SELECT route, COUNT(*) as incident_count
@@ -130,15 +102,7 @@ FROM (
   GROUP BY route
 ) as route_incidents;
 
--- Q19. Analyze the gender distribution in different types of incidents.
-SELECT 
-    v.victims_sex, i.incident_event_type, COUNT(*) AS total_victims
-FROM incidents i
-JOIN victims v ON i.victim_id = v.victim_id
-GROUP BY v.victims_sex, i.incident_event_type
-ORDER BY total_victims DESC;
-
--- Q20. Analyze the frequency of incidents involving multiple victims.
+-- Q15. Analyze the frequency of incidents involving multiple victims.
 SELECT i.date_of_incident, o.operator, COUNT(*) AS victim_count
 FROM incidents i
 JOIN operators o ON o.operator_id = i.operator_id
